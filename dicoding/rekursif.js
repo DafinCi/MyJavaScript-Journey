f2de8911b887394a00266ef8ade399bcbdc5f5e3
DapinCi <alfarizimuhammaddafin@gmail.com>
2026-01-19 14:23:16 +0700

    lanjut belajar FP di dicoding

--- dicoding/rekursif.js
+++ dicoding/rekursif.js
@@ -11,3 +11,8 @@ function generateArr(n) {
 }
 
 console.log(generateArr(10));
+
+// rekursi adalah tenik yang manjur
+// yang jadi tantangan adalah kita harus membiasakan berpikir berbeda, yang awalnya imperatif jadi deklaratif
+
+// untungnya JS sudah menyediakan method kek .map, .filter, forEach dibalik layar cara kerja mereka sama sepert rekursif yaitu immutability
